40e6583181dff63e3f1cd4a9d2ac382309df01a6
Satria Sapta <48681521+satriasapta@users.noreply.github.com>
2021-12-27 15:58:18 +0700

    Update DBHelper.java

--- src/koperasisimpanpinjam/db/DBHelper.java
+++ src/koperasisimpanpinjam/db/DBHelper.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package koperasisimpanpinjam.db;
 
 import java.sql.Connection;
@@ -24,17 +20,6 @@ public class DBHelper {
     public static Connection getConnection(String driver) throws SQLException {
         Connection conn = null;
         switch (driver) {
-            case "SQLITE": {
-                try {
-                    Class.forName("org.sqlite.JDBC");
-                    conn = DriverManager.getConnection(SQCONN);
-                    createTable(conn, driver);
-                } catch (ClassNotFoundException ex) {
-                    System.out.println("Library tidak ada");
-                    Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                break;
-            }
             case "MYSQL": {
                 try {
                     Class.forName("com.mysql.jdbc.Driver");
@@ -84,33 +69,7 @@ public class DBHelper {
                         + ") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
                 break;
             }
-            case "SQLITE": {
-                sqlCreate = "CREATE TABLE IF NOT EXISTS  nasabah ("
-                        + "    rekening INT (10)      PRIMARY KEY ,"
-                        + "    nama      VARCHAR (100),"
-                        + "    alamat   VARCHAR (100) "
-                        + ");"
-                        + "CREATE TABLE IF NOT EXISTS rekening ("
-                        + "    noRekening INT (10)       PRIMARY KEY,"
-                        + "    saldo    DOUBLE (16, 2),"
-                        + "    rekening  INT (10)        REFERENCES nasabah (rekening) ON DELETE RESTRICT"
-                        + "                                                                    ON UPDATE CASCADE"
-                        + ");"
-                        + "CREATE TABLE IF NOT EXISTS perusahaan ("
-                        + "    rekening INT (10)      PRIMARY KEY "
-                        + "                            REFERENCES nasabah (rekening) ON DELETE RESTRICT"
-                        + "                                                                  ON UPDATE CASCADE,"
-                        + "    nib   VARCHAR (100) "
-                        + ");"
-                        + "CREATE TABLE IF NOT EXISTS  individu ("
-                        + "    rekening INT (10)     PRIMARY KEY "
-                        + "                           REFERENCES nasabah (rekening) ON DELETE RESTRICT"
-                        + "                                                                 ON UPDATE CASCADE,"
-                        + "    nik    BIGINT (255),"
-                        + "    npwp BIGINT(255)"
-                        + ");";
-                break;
-            }
+
         }
         String sqls[] = sqlCreate.split(";");
         for (String sql : sqls) {
